ed4302f405bcf35c136cef89fe232462f6608a24
Ti-Chung Cheng <tcheng10@illinois.edu>
2023-02-08 10:22:24 -0600

    fix init fetch

--- src/App.tsx
+++ src/App.tsx
@@ -1,28 +1,25 @@
 import './App.css';
 import TestPage from './pages/test-page';
-import { useEffect } from 'react';
+import { useEffect, useState } from 'react';
 import { useDispatch, useSelector } from 'react-redux';
 import { fetchMetaData } from './features/metadataSlice';
 import { fetchSampleQuestions } from './features/questionsSlice';
 import { fetchSampleOptions } from './features/qvOptionsSlice';
 
 const App = () => {
-  const { metadataLoaded } = useSelector(state => state.metadata.loaded);
-  const { qvOptionsLoaded } = useSelector(state => state.qvOptions.loaded);
-  const { questionsLoaded } = useSelector(state => state.questions.loaded);
+  const dispatch = useDispatch();
+  const metadataLoaded = useSelector(state => state.metadata.loaded);
+  const qvOptionsLoaded = useSelector(state => state.qvOptions.loaded);
+  const questionsLoaded = useSelector(state => state.questions.loaded);
 
   useEffect(() => {
     const fetchData = () => {
-      fetchMetaData();
-      fetchSampleQuestions();
-      fetchSampleOptions();
+      dispatch(fetchMetaData());
+      dispatch(fetchSampleQuestions());
+      dispatch(fetchSampleOptions());
     };
     fetchData();
-  }, []);
-
-  if (metadataLoaded && questionsLoaded && qvOptionsLoaded) {
-    console.log('All data loaded');
-  }
+  }, [dispatch]);
 
   if (!metadataLoaded || !questionsLoaded || !qvOptionsLoaded) {
     return <div>Loading...</div>;
@@ -32,3 +29,6 @@ const App = () => {
 };
 
 export default App;
+
+
+
