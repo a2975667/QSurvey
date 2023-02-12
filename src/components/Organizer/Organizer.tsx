2a41d231a6163d63ea0e1e0536e6764ec79a38d2
Ti-Chung Cheng <tcheng10@illinois.edu>
2023-02-12 16:02:17 -0600

    working category

--- src/components/Organizer/Organizer.tsx
+++ src/components/Organizer/Organizer.tsx
@@ -1,10 +1,110 @@
-// import { DropdownDesign } from "./DropdownDesign";
-// import { DropdownDesign } from "./DropdownDesign";
 import React, { useEffect, useState } from "react";
-import { updateOptionVotes } from "../../features/qvOptionsSlice";
+import { updateOptionGroup, updateOptionVotes } from "../../features/qvOptionsSlice";
 import { useDispatch } from 'react-redux';
 import { IQvOption } from "../../types/coreTypes";
+import "./organizer.css";
 
+interface OrganizerType {
+    options: {[key: string]: IQvOption};
+  }
+
+export const Organizer = (props: OrganizerType) => {
+    // the order here matters, the last one must be undecided.
+    const selfDefinedCategories = ["Positive", "Neutral", "Negative"];
+    const categories = selfDefinedCategories.concat(["Undecided"]);
+    const optionsByCategory = {} as { [key: string]: IQvOption[]}
+
+    categories.forEach(category => {
+        optionsByCategory[category] = [];
+    });
+
+    Object.values(props.options).forEach(option => {
+        const category = categories.includes(option.group) ? option.group : "Undecided";
+        // console.log(option.optionId, category);
+        // console.log(optionsByCategory);
+        optionsByCategory[category].push(option);
+      });
+
+
+    const dispatch = useDispatch();
+
+    const updateGroupByOptionId = (optionId: string, newGroup: string) => {
+        dispatch(updateOptionGroup({ optionId, newGroup }));
+    };
+
+    const resetGroupByOptionId = (optionId: string, newGroup: string) => {
+        newGroup = "Undefined";
+        dispatch(updateOptionGroup({ optionId, newGroup}));
+    };
+
+    return (
+        <div>
+            <h1>Organizer</h1>
+            <div className="container">
+                {categories.slice(0, 3).map(category => (
+                    <div className={`category ${category.toLowerCase()}`} key={category}>
+                        <h2>{category}</h2>
+
+                        {optionsByCategory[category].map(option => (
+                            <div className="option-box" key={option.optionId}>
+                                <div className="option-name">{option.optionName}</div>
+                                <div className="option-buttons">
+                                <button 
+                                        key="Undefined"
+                                        onClick={() => updateGroupByOptionId(option.optionId, "Undefined")}
+                                    > Redecide
+                                    </button>
+                                </div>
+                            </div>
+                        ))}
+                    </div>
+                ))}
+            </div>
+
+            <div className="undecided">
+                <h2>Undecided</h2>
+                {optionsByCategory["Undecided"].map(option => (
+                    <div className="option-box" key={option.optionId}>
+                        <div className="option-name">{option.optionName}</div>
+                        <div className="option-buttons">
+                        {selfDefinedCategories.map(selfDefinedCategory => (
+                            <button 
+                                key={selfDefinedCategory}
+                                onClick={() => updateGroupByOptionId(option.optionId, selfDefinedCategory)}
+                            >
+                                {selfDefinedCategory}
+                            </button>
+                        ))}
+                        </div>
+                    </div>
+                ))}
+            </div>
+        </div>
+    );
+};
+
+
+
+
+        
+        
+
+        // <select value={selectedOption} onChange={handleOptionChange}>
+        //     {renderDropdownOptions(votingOptions)}
+        // </select>    
+    // const [selectedOption, setSelectedOption] = useState(props.currVote);
+
+    // useEffect(() => {
+    //     setSelectedOption(props.currVote);
+    // }, [props.currVote]);
+
+    // const handleOptionChange = (e: React.ChangeEvent<HTMLSelectElement>) => {
+    //     const newVote = Number(e.target.value);
+    //     updateQvOption(dispatch, props.optionId, newVote);
+    //     setSelectedOption(newVote);
+    // };
+
+    
 // const createDropdownOptions = (currentVote: number, currCost: number) => {
 //     console.log(currentVote, currCost)
 //     const maxVote = Math.floor(Math.sqrt(Math.pow(currentVote, 2) + currCost));
@@ -31,46 +131,3 @@ import { IQvOption } from "../../types/coreTypes";
 //         updateOptionVotes({optionId, newVote})
 //     );
 // };
-
-export const Organizer = (options: { [key: string]: IQvOption }) => {
-    console.log(options);
-    // the order here matters
-    const categories = ["Positive", "Negative", "Neutral", "Undecided"];
-    const optionsByCategory = {
-        Positive: [],
-        Negative: [],
-        Neutral: [],
-        Undecided: []
-    }
-    // create a dictionary where key is one of the categories and value is a list of options
-    // const addOptionsToCategories = (options: IQvOption[]) => {
-    //     options.forEach((option) => {
-    //         optionsByCategory[option.group].push(option);
-    //     });
-    // }
-    
-    // addOptionsToCategories(options);
-
-
-    const dispatch = useDispatch();
-    
-    // const [selectedOption, setSelectedOption] = useState(props.currVote);
-
-    // useEffect(() => {
-    //     setSelectedOption(props.currVote);
-    // }, [props.currVote]);
-
-    // const handleOptionChange = (e: React.ChangeEvent<HTMLSelectElement>) => {
-    //     const newVote = Number(e.target.value);
-    //     updateQvOption(dispatch, props.optionId, newVote);
-    //     setSelectedOption(newVote);
-    // };
-
-    return (
-        <h1>Here is the organizer</h1>        
-
-        // <select value={selectedOption} onChange={handleOptionChange}>
-        //     {renderDropdownOptions(votingOptions)}
-        // </select>
-    );
-};
