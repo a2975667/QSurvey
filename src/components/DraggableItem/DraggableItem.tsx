fa054bfb7ba511a4e2679dc1993d49010c465492
Ti-Chung Cheng <tcheng10@illinois.edu>
2023-02-16 21:54:01 -0600

    finish wheel

--- src/components/DraggableItem/DraggableItem.tsx
+++ src/components/DraggableItem/DraggableItem.tsx
@@ -65,7 +65,7 @@ export const DraggableItem = (props: DraggableItemProps) => {
 
                 {isHovering && (
                   <VoteSelection
-                    designType="Drop"
+                    designType="Wheel"
                     optionId={props.option.optionId}
                     currVote={props.option.votes}
                     totalCredits={props.totalCredits!}
