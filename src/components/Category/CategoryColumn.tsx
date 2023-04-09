57e81a2a5c9a31caa5749b21a2a3783b00c73546
Yutong Zhang <yutongz7@illinois.edu>
2023-04-09 12:03:16 -0500

    fix the bug of vote title

--- src/components/Category/CategoryColumn.tsx
+++ src/components/Category/CategoryColumn.tsx
@@ -108,25 +108,25 @@ export const CategoryColumn = (props: CategoryColumnProps) => {
       {/* Vote View */}
       {props.view === "vote" && props.category === "Positive" && (
         <div className="viewCategoryTitle-positive">
-          <h2 className="title">Lean Positive Options</h2>
+          <h2 className="viewCategoryTitle-title">Lean Positive Options</h2>
           <CustomButton className={"reorder"} label="Reorder" onClick={() => reorderCategoryOptions(props.category)} />
         </div>
       )}
       {props.view === "vote" && props.category === "Neutral" && (
         <div className="viewCategoryTitle-neutral">
-          <h2 className="title">Lean Neutral Options</h2>
+          <h2 className="viewCategoryTitle-title">Lean Neutral Options</h2>
           <CustomButton className={"reorder"} label="Reorder" onClick={() => reorderCategoryOptions(props.category)} />
         </div>
       )}
       {props.view === "vote" && props.category === "Negative" && (
         <div className="viewCategoryTitle-negative">
-          <h2 className="title">Lean Negative Options</h2>
+          <h2 className="viewCategoryTitle-title">Lean Negative Options</h2>
           <CustomButton className={"reorder"} label="Reorder" onClick={() => reorderCategoryOptions(props.category)} />
         </div>
       )}
       {props.view === "vote" && props.category === "Skip" && (
         <div className="viewCategoryTitle-undecided">
-          <h2 className="title">Skipped or Undecided Options</h2>
+          <h2 className="viewCategoryTitle-title">Skipped or Undecided Options</h2>
           <CustomButton className={"reorder"} label="Reorder" onClick={() => reorderCategoryOptions(props.category)} />
         </div>
       )}
