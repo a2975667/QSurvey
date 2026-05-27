c45ea4629ee8b480b54a85e75f41821a741f9c5b
Ti-Chung Cheng <tcheng10@illinois.edu>
2026-05-27 15:35:29 -0500

    Use source wording for GitHub footer badge

--- client/src/components/footer/Footer.test.tsx
+++ client/src/components/footer/Footer.test.tsx
@@ -2,14 +2,14 @@ import { render, screen } from '@testing-library/react';
 import Footer from './Footer';
 
 describe('Footer', () => {
-  it('links to the open source GitHub repository', () => {
+  it('links to the GitHub source repository', () => {
     render(<Footer />);
 
     const badge = screen.getByRole('link', {
-      name: 'QSurvey open source repository on GitHub',
+      name: 'View QSurvey source on GitHub',
     });
 
-    expect(badge).toHaveTextContent('Open source on GitHub');
+    expect(badge).toHaveTextContent('View source on GitHub');
     expect(badge).toHaveAttribute('href', 'https://github.com/a2975667/QSurvey');
     expect(badge).toHaveAttribute('target', '_blank');
     expect(badge).toHaveAttribute('rel', 'noopener noreferrer');
